d35c7f10704e2e5c7d0bf881aa87d45abe6a6832
psychopomp7 <mridulhemrajani7@gmail.com>
2026-05-20 18:04:33 +0530

    initial commit

---
+++ src/data/story.js
@@ -0,0 +1,28 @@
+import img1 from "../assets/images/img1.jpeg";
+import img2 from "../assets/images/img2.jpeg";
+import img3 from "../assets/images/img3.jpeg";
+import img4 from "../assets/images/img4.jpeg";
+import img5 from "../assets/images/img5.jpeg";
+
+export const story = [
+  {
+    image: img1,
+    text: "This girl is Vidushi!",
+  },
+  {
+    image: img2,
+    text: "The first time i met her was back in 2022, and we have come so very far since then.",
+  },
+  {
+    image: img3,
+    text: "The second time we met, with a lots of question, whose answer we both know was love",
+  },
+  {
+    image: img4,
+    text: "Our most recent meet-up. The best birthday gift i ever recieved.",
+  },
+  {
+    image: img5,
+    text: "I am a dumbass, and often do things that make you sad and mad. But i want you to know, that anytime my actions splash in like a wave on the sand, I will always carve V <3 M on the sand again, just like you did. I am sorry, Vidushi",
+  },
+];
